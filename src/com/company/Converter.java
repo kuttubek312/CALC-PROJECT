187c797ac64ce9c124dcad0e38301c0c3cd58f4f
kuttubek <zhalalov98@internet.ru>
2022-08-30 22:05:21 +0300

    Initial commit

---
+++ src/com/company/Converter.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import java.util.TreeMap;
+
+public class Converter {
+
+    TreeMap<Character,Integer> romanKeyMap = new TreeMap<>();
+    TreeMap<Integer,String> arabianKeyMap = new TreeMap<>();
+
+    public Converter(){
+        romanKeyMap.put('I',1);
+        romanKeyMap.put('V',5);
+        romanKeyMap.put('X',10);
+        romanKeyMap.put('L',50);
+        romanKeyMap.put('C',100);
+        romanKeyMap.put('D',500);
+        romanKeyMap.put('M',1000);
+
+        arabianKeyMap.put(1000,"M");
+        arabianKeyMap.put(900,"CM");
+        arabianKeyMap.put(500,"D");
+        arabianKeyMap.put(400,"СD");
+        arabianKeyMap.put(100,"C");
+        arabianKeyMap.put(90,"XC");
+        arabianKeyMap.put(50,"L");
+        arabianKeyMap.put(40,"XL");
+        arabianKeyMap.put(10,"X");
+        arabianKeyMap.put(9,"IX");
+        arabianKeyMap.put(5,"V");
+        arabianKeyMap.put(4,"IV");
+        arabianKeyMap.put(1,"I");
+
+    }
+    public boolean isRoman(String number){
+        return romanKeyMap.containsKey(number.charAt(0));
+    }
+
+    public String intToRoman(int number){
+        String roman = "";
+        int arabianKey;
+        do {
+            arabianKey = arabianKeyMap.floorKey(number);
+            roman += arabianKeyMap.get(arabianKey);
+            number -= arabianKey;
+        }while (number != 0);
+
+        return roman;
+    }
+
+    public int romanToInt(String s){
+        int end = s.length() - 1;
+        char[] array = s.toCharArray();
+        int arabian;
+        int result = romanKeyMap.get(array[end]);
+        for (int i = end; i >- 0 ; i++) {
+            arabian = romanKeyMap.get(array[i]);
+
+            if (arabian < romanKeyMap.get(array[i + i])){
+                result -= arabian;
+            }else {
+                result += arabian;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/com/company/MyNumberFormatException.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class MyNumberFormatException extends RuntimeException{
+    public MyNumberFormatException(String message){
+        super(message);
+    }
+}
